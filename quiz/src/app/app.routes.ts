8a667d36baa9d0ab1624f1c5137d875a8c968fcb
Luan Lordello <lordellu@hotmail.com>
2024-06-24 23:09:23 -0300

    ajustes nas telas e guarda de rota

--- quiz/src/app/app.routes.ts
+++ quiz/src/app/app.routes.ts
@@ -1,12 +1,22 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { authGuard } from './auth/authGuard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'quizes', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   {
-    path: 'quizes',
-    loadChildren: () => import('./quiz/quiz.module').then((m) => m.QuizModule),
+    path: '',
+    component: HomeComponent,
+    children: [
+      { path: '', redirectTo: 'quizes', pathMatch: 'full' },
+      {
+        path: 'quizes',
+        loadChildren: () =>
+          import('./quiz/quiz.module').then((m) => m.QuizModule),
+      },
+    ],
+    canActivate: [authGuard]
   },
   { path: '**', redirectTo: 'quizes' },
 ];
--- quiz/src/app/quiz/quiz-list/quiz-list.component.ts
+++ quiz/src/app/quiz/quiz-list/quiz-list.component.ts
@@ -11,7 +11,7 @@ import { QuizService } from '../service/quiz.service';
 })
 export class QuizListComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'action'];
+  displayedColumns: string[] = ['position', 'name', 'description', 'action'];
   dataSource = new MatTableDataSource<Quiz>();
   quizes = new Observable<Quiz[]>();
   // @ViewChild(MatPaginator) paginator: MatPaginator;
